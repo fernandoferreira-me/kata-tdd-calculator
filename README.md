cb77836e96c08ccef974922a09f85f265ea861b3
Fernando Ferreira <fernandogferreira@gmail.com>
2019-10-17 19:56:30 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kata TDD
+
+https://static1.squarespace.com/static/5c741968bfba3e13975e33a6/t/5ca6614d971a1877cadc4f8a/1554407757512/String+Calculator+Kata+v1.pdf
---
+++ src/StringCalculator.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 fguimara <fguimara@Fernandos-MacBook-Pro.local>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Kata TDD String Calculator
+"""
+
+import unittest
+
+
+class StringCalculator(object):
+    """
+    Class for implementing a calculator based on strings
+    """
+    def add(number_string):
+        """
+        The add function of the calculator
+        """
+        pass
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
